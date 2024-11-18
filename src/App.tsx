b9cf0a9623aa29e8a4051cc1377baa2342467010
Illmix <illachaban1100@gmail.com>
2024-11-18 17:52:56 +0200

    layout style and variables change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Layout from "./layout/Layout.tsx";
+import Header from "./sections/header/Header.tsx";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Layout>
+      <Header/>
+    </Layout>
   )
 }
 
