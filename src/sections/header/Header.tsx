d9d56b5cc7d7c0ba3075b76d40fff936359989a9
Illmix <illachaban1100@gmail.com>
2024-11-18 16:50:58 +0200

    create header info component

---
+++ src/sections/header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderInfo from "./components/HeaderInfo.tsx";
+
+const Header = () => {
+    return (
+        <div>
+            <HeaderInfo/>
+        </div>
+    );
+};
+
+export default Header;
