fd06276925a8a0dd3315b4cd6f4800c3becd8b6d
Illmix <illachaban1100@gmail.com>
2024-11-20 00:57:31 +0200

    create navigation menu component

---
+++ src/sections/nav-menu/NavMenu.tsx
@@ -0,0 +1,23 @@
+const NavMenu = () => {
+    return (
+        <ul>
+            <li>
+                <a>
+                    About
+                </a>
+            </li>
+            <li>
+                <a>
+                    Experience
+                </a>
+            </li>
+            <li>
+                <a>
+                    Projects
+                </a>
+            </li>
+        </ul>
+    );
+};
+
+export default NavMenu;
