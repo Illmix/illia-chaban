a5b186e3f03076095c5dc49cce051c6b813cce4a
Illmix <illachaban1100@gmail.com>
2024-11-21 21:05:16 +0200

    create summary section

---
+++ src/sections/right-side/components/Summary.tsx
@@ -0,0 +1,30 @@
+const Summary = () => {
+    return (
+        <div className="summary-container">
+            <p className="summary-text">
+                I'm a 20 year old fullstack developer with 2 years of full-time experience. Proficient in both front-end and back-end devel-
+                opment, with expertise in React, Next.js, TypeScript, Flask, MongoDB. Strong problem-solving abilities, a keen eye for
+                detail, and dedicated to creating robust and scalable applications.
+                I grow through challenges and strengthen through repetition.
+            </p>
+            <p className="summary-text">
+                Relocated to Germany in 2022 due to the Russian invasion of Ukraine. Successfully completed an
+                Associate Degree in Computer Science remotely from a Ukrainian college. Subsequently, joined <strong className="heading-color">HomeKlick
+                GmbH</strong>, a small IT company, where I worked as a <strong className="heading-color">Full Stack Developer</strong> for nearly
+                two years. My
+                role primarily focused
+                on <strong className="heading-color">frontend development (80%)</strong> with additional backend responsibilities (20%). Key
+                contributions included
+                developing
+                and maintaining <strong className="heading-color">dynamic, responsive websites</strong> enhancing user interfaces to ensure
+                seamless interactions,
+                and optimizing APIs and database structures to improve system reliability and scalability.
+            </p>
+            <p className="summary-text">
+                In my spare time, I’m usually hanging out with my friends, reading or playing video games.
+            </p>
+        </div>
+    );
+};
+
+export default Summary;
