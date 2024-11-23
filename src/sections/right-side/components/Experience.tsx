bef306069045d1545dde2be7f6f4e0b85b4259dc
Illmix <illachaban1100@gmail.com>
2024-11-23 17:10:34 +0200

    add resume to the experience section

--- src/sections/right-side/components/Experience.tsx
+++ src/sections/right-side/components/Experience.tsx
@@ -1,6 +1,6 @@
 import {experiences} from "../../../config.ts";
 import WorkSectionItem from "../../../components/WorkSectionItem.tsx";
-
+import resume from "../../../assets/Resume.pdf"
 const Experience = () => {
     return (
         <div className="experience-container">
@@ -13,6 +13,17 @@ const Experience = () => {
                     date={e.date}
                 />
             )}
+            <a className="resume-link heading" href={resume}><h3><span>View Full Resume</span><span>
+                <svg
+                    width="1rem" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="animate"
+                    aria-hidden="true"><path
+                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                    clip-rule="evenodd"></path></svg>
+            </span></h3>
+            </a>
         </div>
     );
 };
